78ecf9b78bc8af633773d847f041c32ecdd5d9ad
Pradhyumnaa <pradhyumnaag666@gmail.com>
2021-06-02 21:18:27 +0700

    Fixing Time in Instructions

--- Cognitive_Science_Projects/Music_Short_Term_Memory/music1.js
+++ Cognitive_Science_Projects/Music_Short_Term_Memory/music1.js
@@ -9,7 +9,7 @@ var button;
 
 var writer;
 
-var timerValue = 20;
+var timerValue = 30;
 
 var song;
 
@@ -112,7 +112,7 @@ function createStartButton(){
     noStroke();
     fill(0,0,0);
     textSize(20);
-    var introText = text("In this experiment, I aim to find out what type of Music Genre aids in Short Term Memory. When you click start, you will be taken to a page with 12 randomly generated words. Your aim is to try and remember as much as you can within 15 seconds. Once the timer is up, you will be taken to a prompt where you will type in as many words as you remember. When you click submit, you will receive the results and will be taken to the next Music Genre. Good Luck!!", width/2 - 600, 300,500,500);
+    var introText = text("In this experiment, I aim to find out what type of Music Genre aids in Short Term Memory. When you click start, you will be taken to a page with 12 randomly generated words. Your aim is to try and remember as much as you can within 30 seconds. Once the timer is up, you will be taken to a prompt where you will type in as many words as you remember. When you click submit, you will receive the results and will be taken to the next Music Genre. Good Luck!!", width/2 - 600, 300,500,500);
     
     button2 = createButton('Start');
     button2.position(width/2,height/2);
